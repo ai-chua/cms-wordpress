de2b638e2497f35a280e19dc5e59986a185b48d1
Ailin Chua <ailin@hodlnaut.com>
2021-01-11 17:22:59 +0800

    Title change

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -32,7 +32,7 @@ export default function Post({ post, posts, preview }) {
             <article>
               <Head>
                 <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
+                  {post.title} | Hodlnaut Blog
                 </title>
                 <meta
                   property="og:image"
